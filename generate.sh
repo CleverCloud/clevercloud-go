56fb57a2c1cd242c9515a534628b7f125496fa3d
Gaël Reyrol <gael@dansmaculotte.fr>
2020-12-11 21:24:31 +0100

    feat: add scripts

---
+++ generate.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+set -eu
+
+SCHEMA_URL="https://api.clever-cloud.com/v2/openapi.json"
+OUTPUT_FOLDER="clevercloud"
+PACKAGE_VERSION="0.1.0"
+
+GOFMT_PATH=$(which gofmt)
+
+export GO_POST_PROCESS_FILE="${GOFMT_PATH} -w"
+
+./openapi-generator.sh generate \
+  -g go \
+  -i "${SCHEMA_URL}" \
+  -o "${OUTPUT_FOLDER}" \
+  --skip-validate-spec \
+  --enable-post-process-file \
+  --minimal-update \
+  --additional-properties=packageName="clevercloud",packageVersion="${PACKAGE_VERSION}"
---
+++ openapi-generator.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+set -e
+
+OPENAPI_GENERATOR_VERSION="4.3.1"
+OPENAPI_GENERATOR_URL="https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_GENERATOR_VERSION}/openapi-generator-cli-${OPENAPI_GENERATOR_VERSION}.jar"
+OPENAPI_GENERATOR_PATH="./openapi-generator-cli.jar"
+
+if [[ ! -f ${OPENAPI_GENERATOR_PATH} ]]; then
+    wget ${OPENAPI_GENERATOR_URL} -O ${OPENAPI_GENERATOR_PATH}
+fi
+
+java -ea \
+  ${JAVA_OPTS} \
+  -Xms512M \
+  -Xmx1024M \
+  -server \
+  -jar ${OPENAPI_GENERATOR_PATH} \
+  "$@"
